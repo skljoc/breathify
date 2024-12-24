94dc77dd8adbc173d3866d0c497f939cfc26e7cc
skljoc <35695094+skljoc@users.noreply.github.com>
2024-12-24 17:21:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let audioPlayer;
+
+function startExercise(type) {
+    const overlay = document.getElementById('overlay');
+    const title = document.getElementById('exercise-title');
+    const guide = document.getElementById('exercise-guide');
+    audioPlayer = document.getElementById('audio-player');
+
+    overlay.style.display = 'flex';
+
+    switch (type) {
+        case 'box':
+            title.innerText = 'Box Breathing';
+            guide.innerText = 'Inhale 4s - Hold 4s - Exhale 4s - Hold 4s';
+            startAnimation(4);
+            playAudio('assets/sounds/box-breathing.mp3');
+            break;
+        case '4-7-8':
+            title.innerText = '4-7-8 Breathing';
+            guide.innerText = 'Inhale 4s - Hold 7s - Exhale 8s';
+            startAnimation(7);
+            playAudio('assets/sounds/4-7-8-breathing.mp3');
+            break;
+        case 'pranayama':
+            title.innerText = 'Pranayama';
+            guide.innerText = 'Inhale deeply and exhale slowly.';
+            startAnimation(5);
+            playAudio('assets/sounds/pranayama.mp3');
+            break;
+    }
+}
+
+function playAudio(filePath) {
+    audioPlayer.src = filePath;
+    audioPlayer.play();
+}
+
+function stopExercise() {
+    document.getElementById('overlay').style.display = 'none';
+    audioPlayer.pause();
+    audioPlayer.currentTime = 0;
+}
